38968f6449ba6a58436d8f7c1666cba0a0b81549
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2018-10-24 14:04:14 +0700

    test send boardJSON

--- index.js
+++ index.js
@@ -7,34 +7,21 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 })
 
-// let clients = [];
-
 io.on('connection', function(socket){
     console.log('one user connected ' + socket.id);
 
     socket.on('req', (message) => {
-        // if(client.length%2 === 0) {
-        //     clients.push({
-        //         player1: socket.id,
-        //         player2: ""
-        //     });
-        //     io.emit("waiting", "waiting for another opponent...")
-        // } else {
-        //     clients[clients.length-1].player2 = socket.id;
-        //     io.emit("connected", "connection success");
-        //     // change to send array of randomed field
-        //     io.to(clients[clients.length-1].player1).emit("start", "start match")
-        // }
-        console.log(message)
+        console.log(socket.id+" "+message)
+        if(message === "join") {
+            io.emit("status", "waiting for players...");
+            io.emit("board", {
+                warderindex: [2,3],
+                prisonerindex: [4,1],
+                tunnelindex: [4,0],
+                obstacleindex: [[0,0], [2,2], [4,2], [1,3], [3,4]]
+            });
+        }
     });
-
-    socket.on('turn', (data) => {
-
-    })
-
-    socket.on('board', () => {
-
-    })
 
 
     socket.on('disconnect', function() {
