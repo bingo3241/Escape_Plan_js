4e2b233e7c228f15aeb5b2b08f2d9e76ea717716
Satsawat Suttawuttiwong <Bingo@Satsawats-MacBook-Pro.local>
2018-10-25 23:23:55 +0700

    Bingo commit change from counting clients in the server to only in the room, remove var clients[]

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var connectionsLimit = 2;
-var clients = [];
 var roles =[];
 // var path = require('path');
 function randomRoles() {
@@ -30,32 +28,31 @@ io.on("connection", function(socket) {
     socket.disconnect()
     console.log(socket.id+ ' has tried to connect but the socket has reach the limit of connections. Disconnected...')
     return
-}
-console.log('one user connected ' + socket.id);
-socket.join('room 1', () => {
+  }
+  console.log('one user connected ' + socket.id);
+  socket.join('room', () => {
     let rooms = Object.keys(socket.rooms);
     console.log(rooms);
-    io.to('room 1').emit(socket.id+" has joined the room")
-    if(io.engine.clientsCount === 1) {
-        clients.push({
-            player1: socket.id,
-            player2: ""
-        });
+    io.to('room').emit(socket.id+" has joined the room")
+    io.of('/').in('room').clients((error, clients) => {
+      if (error) throw error;
+      if(clients.length === 1) {
+        console.log("Client(s) in the room: "+clients);
         io.emit("waiting", "waiting for another opponent...");
-    } else if(io.engine.clientsCount === 2) {
-        clients[clients.length-1].player2 = socket.id;
-        console.log(clients);
+      } else if(clients.length === 2) {
+        console.log("Client(s) in the room: "+clients);
         io.emit("connected", "connection success");
         randomRoles();
-        io.to(clients[0].player1).emit("char",roles[0].player1);
-        console.log("Player1 is "+roles[0].player1);
-        io.to(clients[0].player2).emit("char",roles[0].player2);
-        console.log("Player2 is "+roles[0].player2);
+        io.to(clients[0]).emit("char",roles[0].player1);
+        console.log("Player1 ("+clients[0]+") is "+roles[0].player1);
+        io.to(clients[1]).emit("char",roles[0].player2);
+        console.log("Player2 ("+clients[1]+") is "+roles[0].player2);
 
         
         // change to send array of randomed field
         io.to(clients[clients.length-1].player1).emit("start", "start match");
-    }
+      }  
+    });
         
 
   // console.log("one user connected " + socket.id);
@@ -83,67 +80,3 @@ socket.join('room 1', () => {
 http.listen(3000, function() {
   console.log("server listening on port 3000");
 });
-=======
-var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
-var connectionsLimit = 2;
-var clients = [];
-// var path = require('path');
-
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
-
-io.on("connection", function(socket) {
-  if (io.engine.clientsCount > connectionsLimit) {
-    socket.emit('err', { message: 'reach the limit of connections' })
-    socket.disconnect()
-    console.log(socket.id+ ' has tried to connect but the socket has reach the limit of connections. Disconnected...')
-    return
-}
-console.log('one user connected ' + socket.id);
-socket.join('room 1', () => {
-    let rooms = Object.keys(socket.rooms);
-    console.log(rooms);
-    io.to('room 1').emit(socket.id+" has joined the room")
-    if(io.engine.clientsCount === 1) {
-        clients.push({
-            player1: socket.id,
-            player2: ""
-        });
-        io.emit("waiting", "waiting for another opponent...");
-    } else {
-        clients[clients.length-1].player2 = socket.id;
-        io.emit("connected", "connection success");
-        // change to send array of randomed field
-        io.to(clients[clients.length-1].player1).emit("start", "start match");
-    }
-        console.log(clients);
-
-  // console.log("one user connected " + socket.id);
-  // console.log("This is from Tai");
-
-  // socket.on("req", message => {
-  //   console.log(socket.id + " " + message);
-  //   if (message === "join") {
-  //     io.emit("status", "waiting for players...");
-  //     // must send random index not
-  //     io.emit("board", {
-  //       warderindex: [2, 3],
-  //       prisonerindex: [4, 1],
-  //       tunnelindex: [4, 0],
-  //       obstacleindex: [[0, 0], [2, 2], [4, 2], [1, 3], [3, 4]]
-  //     });
-  //   }
-  });
-
-  socket.on("disconnect", function() {
-    console.log("one user disconnected " + socket.id);
-  });
-});
-
-http.listen(3000, function() {
-  console.log("server listening on port 3000");
-});
->>>>>>> 5c9e4892ff326a131d3d880df5334ae868891b45
